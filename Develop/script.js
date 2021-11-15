9e0bc2f4fdb5adc1baf2d8306cfca02e99038c77
Paper Girl <jamie.rose.528@gmail.com>
2021-11-14 16:19:21 -0800

    added password size rules

--- Develop/script.js
+++ Develop/script.js
@@ -11,10 +11,32 @@ function writePassword() {
 }
 
 // include lowercase, uppercase, numeric, and/or special characters
-let lowercase = "abcefghijklmnopqrstuvwxyz"
-let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let numeric = "0123456789"
-let special = "!@#$%^&*()-_=+{}[]':;/?\|~`,.<>"
+var lowercase = "abcefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "0123456789"
+var special = "!@#$%^&*()-_=+{}[]':;/?\|~`,.<>"
+
+function generatePassword() {
+  var characters =" "
+  var password = " "
+
+  var passwordSize = prompt("Length of password", "16")
+  if(!passwordSize) return
+
+  if(isNaN(passwordSize)) {
+    alert ("Enter only numbers.")
+    return
+  }
+
+  if(passwordSize < 8) {
+    alert("Password must be at least 8 characters.")
+    return
+  }
+
+  if (passwordSize > 128) {
+    alert("Password cannot be longer than 128 characters.")
+  }
+}
 
 
 
